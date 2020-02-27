56cbce9d4986b2a24dd20cba5b2a0330af7ed5d1
Laurent Yovanovitch <laurent.yovanovitch@outlook.fr>
2020-02-27 16:35:10 +0100

    Event in function

--- enter-expense/index.js
+++ enter-expense/index.js
@@ -1,16 +1,23 @@
 exports.handler = async (event) => {
-    console.log(event);
-    const receipt = {
-      issuer : event.issuer,
-      expenseDate : event.expenseDate,
-      description : event.description,
-      amount : event.amount,
-      currency : event.currency,
-      location: event.location
-    };
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(receipt),
-    };
-    return response;
+  console.log(event);
+  var eventBody = JSON.parse(event.body);
+  console.log(eventBody);
+
+  const receipt = {
+    issuer: eventBody.issuer,
+    expenseDate: eventBody.expenseDate,
+    description: eventBody.description,
+    amount: eventBody.amount,
+    currency: eventBody.currency,
+    location: eventBody.location
+  };
+
+  console.log(receipt);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(receipt),
+  };
+
+  return response;
 };
